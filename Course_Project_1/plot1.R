1e0e48625d4cbbe42e45e3df4799ddd40315d1d8
govinddata <30706303+govinddata@users.noreply.github.com>
2017-08-08 20:06:18 -0400

    Update plot1.R

--- Course_Project_1/plot1.R
+++ Course_Project_1/plot1.R
@@ -1,11 +1,21 @@
-epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
-epc$DateTime = paste(epc$Date, epc$Time)
-epc$DateTime = as.POSIXlt(epc$DateTime,format="%d/%m/%Y %H:%M:%S")
-epc$Date = NULL
-epc$Time = NULL
+## Getting full dataset
+igdata <- read.csv("./exploratory-data-analysis/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+igdata$Date <- as.Date(igdata$Date, format="%d/%m/%Y")
 
-subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+## Subsetting the data
+subdata <- subset(igdata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(igdata)
 
-png("PNG/plot1.png")
-hist(subepc$Global_active_power,main="Global Active Power",ylab="Frequency",xlab="Global Active Power (kilowatts)",col="red",ylim=c(0,1200))
+## Converting dates
+data_ig <- paste(as.Date(subdata$Date), subdata$Time)
+subdata$Datetime <- as.POSIXct(data_ig)
+
+## Plot 1
+hist(subdata$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="./exploratory-data-analysis/Course_Project_1/figure/plot1.png", height=480, width=480)
 dev.off()
+
