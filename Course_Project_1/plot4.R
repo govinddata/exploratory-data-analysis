6057644f6a533517eaecfc104f28a72989434799
govinddata <30706303+govinddata@users.noreply.github.com>
2017-08-08 20:07:34 -0400

    Update plot4.R

--- Course_Project_1/plot4.R
+++ Course_Project_1/plot4.R
@@ -1,27 +1,33 @@
-epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
-epc$DateTime = paste(epc$Date, epc$Time)
-epc$DateTime = as.POSIXlt(epc$DateTime,format="%d/%m/%Y %H:%M:%S")
-epc$Date = NULL
-epc$Time = NULL
+## Getting full dataset
+ig <- read.csv("./exploratory-data-analysis/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+ig$Date <- as.Date(ig$Date, format="%d/%m/%Y")
 
-subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+## Subsetting the data
+subdata <- subset(ig, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(ig)
 
-env <- par()
+## Converting dates
+igdata <- paste(as.Date(subdata$Date), subdata$Time)
+subdata$Datetime <- as.POSIXct(igdata)
 
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(subdata, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
 
-png("PNG/plot4.png")
-par(mfrow=c(2,2))
-plot(x=(subepc$DateTime),y=subepc$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
-
-plot(x=(subepc$DateTime),y=subepc$Voltage,type="l",ylab="Voltage",xlab="datetime")
-
-plot(x=(subepc$DateTime),y=subepc$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
-lines(x=(subepc$DateTime),y=subepc$Sub_metering_2,col="red")
-lines(x=(subepc$DateTime),y=subepc$Sub_metering_3,col="blue")
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",col=c("black","red","blue"),lwd=2,cex=0.4)
-
-plot(subepc$DateTime, subepc$Global_reactive_power, type = "l", main = "", xlab = "datetime")
-
-par(env)
+## Saving to file
+dev.copy(png, file="./exploratory-data-analysis/Course_Project_1/figure/plot4.png", height=480, width=480)
 dev.off()
-
