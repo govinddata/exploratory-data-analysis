0ff68e06bb3903a85351d2bdfc99e4100c9860f0
govinddata <30706303+govinddata@users.noreply.github.com>
2017-08-08 20:07:14 -0400

    Update plot3.R

--- Course_Project_1/plot3.R
+++ Course_Project_1/plot3.R
@@ -1,14 +1,26 @@
-epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
-epc$DateTime = paste(epc$Date, epc$Time)
-epc$DateTime = as.POSIXlt(epc$DateTime,format="%d/%m/%Y %H:%M:%S")
-epc$Date = NULL
-epc$Time = NULL
+## Getting full dataset
+ig <- read.csv("./exploratory-data-analysis/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+ig$Date <- as.Date(ig$Date, format="%d/%m/%Y")
 
-subepc <- subset(epc,DateTime$year==107 & DateTime$mon==1 & (DateTime$mday==1 | DateTime$mday==2))
+## Subsetting the data
+subdata <- subset(ig, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(ig)
 
-png("PNG/plot3.png")
-plot(x=(subepc$DateTime),y=subepc$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
-lines(x=(subepc$DateTime),y=subepc$Sub_metering_2,col="red")
-lines(x=(subepc$DateTime),y=subepc$Sub_metering_3,col="blue")
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="l",col=c("black","red","blue"),lwd=2,cex=0.7)
+## Converting dates
+igdata <- paste(as.Date(subdata$Date), subdata$Time)
+subdata$Datetime <- as.POSIXct(igdata)
+
+## Plot 3
+with(subdata, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving to file
+dev.copy(png, file="./exploratory-data-analysis/Course_Project_1/figure/plot3.png", height=480, width=480)
 dev.off()
